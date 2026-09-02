ae0affa01977ddbd754e2919604de244bb647bfc
Biplov Gyawali <biplovg@metropolia.fi>
2026-09-02 15:50:07 +0300

    feat: add week 3 frontend pair programming

---
+++ week3-fe-pair-programming/src/App.jsx
@@ -0,0 +1,18 @@
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
